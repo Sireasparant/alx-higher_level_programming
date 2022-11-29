0a7add1af216dec77b5afd5a8801168f9d8932e0
Sireasparant <ugarhutogbo@gmail.com>
2022-11-29 06:04:51 -0800

    check

--- 0x00-python-hello_world/list.h
+++ 0x00-python-hello_world/list.h
@@ -7,7 +7,7 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
 
 size_t print_listint(const list_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
