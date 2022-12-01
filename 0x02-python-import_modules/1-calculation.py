694f49dc5edce96590fe74beda51449e76c95e24
Sireasparant <ugarhutogbo@gmail.com>
2022-12-01 09:57:40 -0800

    import

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """print the sum, difference, mul and quotient of 10 amd 5"""
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sy
+
+    total = 
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+        print("{}".format(total))
