ef1871841a8681f95682e6e8dc8d635964a094e9
Sireasparant <ugarhutogbo@gmail.com>
2022-12-01 10:32:12 -0800

    import1

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Handle basic arithematic operation."""
+    from calculator_1 import add, sub, mul, div
+    import sys
+    if len(sys.argv) -1 != 3:
+        print("usage: ./low-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
